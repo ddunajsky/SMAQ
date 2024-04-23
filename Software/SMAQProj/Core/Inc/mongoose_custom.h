20fa97be6dcf36699e3ab5fb4febcac6c02ecfda
David <dunajsd@wwu.edu>
2024-04-23 13:52:45 -0700

    f4 code

---
+++ Software/SMAQProj/Core/Inc/mongoose_custom.h
@@ -0,0 +1,30 @@
+/*
+ * mongoose_custom.h
+ *
+ *  Created on: Feb 7, 2024
+ *      Author: JJohn
+ */
+
+#ifndef INC_MONGOOSE_CUSTOM_H_
+#define INC_MONGOOSE_CUSTOM_H_
+
+
+#define MG_STMPACK_ARCH MG_ARCH_NEWLIB
+
+
+#define MG_ENABLE_PACKED_FS  1
+#define MG_ENABLE_CUSTOM_MILLIS  0
+#define MG_ENABLE_MBEDTLS  0
+#define MG_ARCH MG_ARCH_CMSIS_RTOS2
+#define MG_ENABLE_CUSTOM_RANDOM  1
+#define MG_ENABLE_DRIVER_STM32  1
+#define MG_ENABLE_TCPIP 1
+#define MG_ENABLE_FILE 0
+
+
+
+
+
+
+
+#endif /* INC_MONGOOSE_CUSTOM_H_ */
